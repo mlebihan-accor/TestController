044870da803c7aca7d90b742c9d2b750d2dd114b
mlebihan-accor <morgan.lebihan@accor.com>
2021-05-11 16:02:07 +0200

    WIP

---
+++ TestController/Controller.swift
@@ -0,0 +1,21 @@
+//
+//  Controller.swift
+//  TestController
+//
+//  Created by mlebihan on 11/05/2021.
+//
+
+import Foundation
+
+
+class Controller {
+
+  // IBAction
+
+  @IBAction func didSelect() {
+    print("<<<<<< didSelect")
+  }
+
+  // textfield
+
+}
---
+++ TestController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestController
+//
+//  Created by mlebihan on 11/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
